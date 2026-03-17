a8f520d2dbc3f710e716faee29f3079abc9e15d0
Aeon <hamsterovich22@gmail.com>
2026-03-17 09:19:21 +0200

    3_drosiba.ps1
    Izveidojiet PowerShell skriptu, kas pārbauda parametrus
    RealTimeProtectionEnabled un QuickScanAge, un izvada paziņojumus
    atbilstoši to parametru vērtībām.
    Ja reāllaika aizsardzība ir izslēgta vai dators nav skenēts vairā nekā 3 dienas,
    skriptam jāizvada sarkanu brīdinājumu “Sistēma ir apdraudēta!”. Citādi
    skriptam jāizvada zaļu paziņojumu “Sistēma ir droša.”

---
+++ 3_drosiba.ps1
@@ -0,0 +1,8 @@
+$status = Get-MpComputerStatus
+$realTimeOn = $status.RealTimeProtectionEnabled
+$daysSinceQuickScan = $status.QuickScanAge
+if ( ($realTimeOn -eq $false) -or ($daysSinceQuickScan -gt 3) ) {
+    Write-Host "Sistema ir apdraudeta!" -ForegroundColor Red
+} else {
+    Write-Host "Sistema ir drosa." -ForegroundColor Green
+}
