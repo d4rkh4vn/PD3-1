c3cf1a61694bd053024a285d070b922d19c42b57
Aeon <hamsterovich22@gmail.com>
2026-03-17 09:30:14 +0200

    2_zurnals.ps1
    Izveidojiet PowerShell skriptu, kas no Event Viewer System žurnāla izgūst
    visas kļūdas, kas notikušas pēdējo 7 dienu laikā, un izvada informāciju par tām
    teksta failā “Errors.txt” aktīvā lietotāja Documents direktorijā.
    Ja skripts atrod vairāk par 10 kļūdām, teksta faila pašā sākumā jāieraksta
    vispārīgo paziņojumu – “[KRITISKI] Sistēma ir nestabila!”. Ja ir mazāk par
    10 kļūdām, tad jāieraksta “[OK] Kļūdu līmenis normāls."
    Zem vispārīgā paziņojuma jāseko kļūdu sarakstam (kļūdas laiks un tai
    atbilstošais ziņojums).

--- 2_zurnals.ps1
+++ 2_zurnals.ps1
@@ -5,17 +5,17 @@ $errors = Get-EventLog -LogName System -EntryType Error -After $sevenDaysAgo
 $errorCount = $errors.Count
 "" | Out-File -FilePath $outputFile -Encoding UTF8
 if ($errorCount -gt 10) {
-"[KRITISKI] Sistema ir nestabila!" | Out-File -FilePath $outputFile -Append -Encoding UTF8
+     "[KRITISKI] Sistema ir nestabila!" | Out-File -FilePath $outputFile -Append -Encoding UTF8
 } else {
-"[OK] Kludu limenis normalss." | Out-File -FilePath $outputFile -Append -Encoding UTF8
+     "[OK] Kludu limenis normalss." | Out-File -FilePath $outputFile -Append -Encoding UTF8
 }
 "" | Out-File -FilePath $outputFile -Append -Encoding UTF8
 if ($errorCount -gt 0) {
-foreach ($e in $errors) {
-$time = $e.TimeGenerated
-$msg = $e.Message
-"$time : $msg" | Out-File -FilePath $outputFile -Append -Encoding UTF8
+     foreach ($e in $errors) {
+          $time = $e.TimeGenerated
+          $msg = $e.Message
+          "$time : $msg" | Out-File -FilePath $outputFile -Append -Encoding UTF8
 }
 } else {
-"Nav kludu." | Out-File -FilePath $outputFile -Append -Encoding UTF8
+     "Nav kludu." | Out-File -FilePath $outputFile -Append -Encoding UTF8
 }
